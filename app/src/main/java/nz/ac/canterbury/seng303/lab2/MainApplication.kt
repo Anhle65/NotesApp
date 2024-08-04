f63e05712e605628800ec4fb70a041747181ae43
ale155 <ale155@uclive.ac.nz>
2024-08-05 11:29:27 +1200

    Starting the app by override onCreate method

--- app/src/main/java/nz/ac/canterbury/seng303/lab2/MainApplication.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/lab2/MainApplication.kt
@@ -1,4 +1,18 @@
 package nz.ac.canterbury.seng303.lab2
 
-class MainApplication {
+import android.app.Application
+import kotlinx.coroutines.FlowPreview
+import nz.ac.canterbury.seng303.lab2.datastore.dataAccessModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class MainApplication : Application() {
+    @OptIn(FlowPreview::class)
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(dataAccessModule)
+        }
+    }
 }
